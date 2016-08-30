cbb06475c10723ab5ac99c5a505f0a1200e5ae24
Yogesh <yogeshumeshvaity@gmail.com>
2016-08-30 13:09:34 +0530

    Refactor PrimeArrayGenerator version 4.

--- Prime Array Generator/src/main/java/PrimeArrayGenerator.java
+++ Prime Array Generator/src/main/java/PrimeArrayGenerator.java
@@ -4,8 +4,9 @@
  */
 class PrimeArrayGenerator {
     private static int size = 0;
-    private static int[] inputArray;
-    private static boolean[] numbersToCrossOut;
+    private static int[] integers;
+    private static boolean[] isCrossed;
+
     static int[] generatePrimes(final int maxValue) {
         if (maxValue < 2) return new int[0];
         initializeSieve(maxValue);
@@ -13,53 +14,66 @@ class PrimeArrayGenerator {
         return getAllPrimes(countPrimes());
     }
 
-    private static void sieve() {
-        crossOutFirstTwoNumbers();
-        crossOutMultiples();
-    }
-
     private static void initializeSieve(final int maxValue) {
         size = maxValue + 1;
 
-        inputArray = fillInputArray(size);
-        numbersToCrossOut = fillCrossOutArray();
+        integers = fillInputArray(size);
+        isCrossed = fillCrossOutArray();
     }
 
     private static int[] getAllPrimes(final int primeCount) {
         int[] primes = new int[primeCount];
-        for (int i = 0, j = 0; i < size; i++) {
-            if (numbersToCrossOut[i]) {
-                primes[j++] = inputArray[i];
+        for (int i = 2, j = 0; i < size; i++) {
+            if (notCrossed(i)) {
+                primes[j++] = integers[i];
             }
         }
         return primes;
     }
 
+    private static boolean notCrossed(int currentInteger) {
+        return isCrossed[currentInteger];
+    }
+
     private static int countPrimes() {
         int primeCount = 0;
-        for (int i = 0; i < size; i++) {
-            if (numbersToCrossOut[i]) {
+        for (int i = 2; i < size; i++) {
+            if (isCrossed[i]) {
                 primeCount++;
             }
         }
         return primeCount;
     }
 
-    private static void crossOutMultiples() {
-        for (int i = 2; i < Math.sqrt(size) + 1; i++) {
-            for (int j = 2 * i; j < size; j += i) {
-                numbersToCrossOut[j] = false;
+    private static void sieve() {
+        int maxPrimeFactor = calcMaxPrimeFactor();
+        for (int i = 2; i < maxPrimeFactor; i++) {
+            // If i is uncrossed, cross its multiples
+            if (isCrossed[i]) {
+                crossOutMultiplesOf(i);
             }
         }
     }
 
-    private static void crossOutFirstTwoNumbers() {
-        numbersToCrossOut[0] = numbersToCrossOut[1] = false;
+    /**
+     * We cross out all the multiples of p, p is prime. Thus all crossed out multiples have p and
+     * q factors. If p > sqrt of the size of the array, then q will never be greater than 1. thus
+     * p is the largest prime factor in the array, and is also the iteration limit.
+     * @return
+     */
+    private static int calcMaxPrimeFactor() {
+        return (int) Math.sqrt(size) + 1;
+    }
+
+    private static void crossOutMultiplesOf(final int i) {
+        for (int j = 2 * i; j < size; j += i) {
+            isCrossed[j] = false;
+        }
     }
 
     private static boolean[] fillCrossOutArray() {
         boolean[] isPrime = new boolean[size];
-        for (int i = 0; i < size; i++) {
+        for (int i = 2; i < size; i++) {
             isPrime[i] = true;
         }
         return isPrime;
