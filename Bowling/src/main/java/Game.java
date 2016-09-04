e1d87bb0f8f971b1be9a1dedae3c67c14399ec29
Yogesh <yogeshumeshvaity@gmail.com>
2016-09-04 16:12:25 +0530

    Refactor scoreForFrame() in Bowling.

--- Bowling/src/main/java/Game.java
+++ Bowling/src/main/java/Game.java
@@ -3,7 +3,8 @@ public class Game {
     private int[] itsThrows = new int[21];
     private int itsCurrentThrow = 0;
     private int itsCurrentFrame = 1;
-    private boolean firstThrow = true;
+    private boolean firstThrowInFrame = true;
+    private int ball;
 
     public int score() {
         return scoreForFrame(getCurrentFrame() - 1);
@@ -16,14 +17,14 @@ public class Game {
     }
 
     private void adjustCurrentFrame(int pins) {
-        if (firstThrow) {
+        if (firstThrowInFrame) {
             if (pins == 10) {
                 itsCurrentFrame++;
             } else {
-                firstThrow = false;
+                firstThrowInFrame = false;
             }
         } else {
-            firstThrow = true;
+            firstThrowInFrame = true;
             itsCurrentFrame++;
         }
         itsCurrentFrame = Math.min(11, itsCurrentFrame);
@@ -31,25 +32,40 @@ public class Game {
 
     public int scoreForFrame(int frame) {
         int score = 0;
-        int frameScore = 0;
-        int ball = 0;
+        ball = 0;
         for (int currentFrame = 0; currentFrame < frame; currentFrame++) {
-            final int firstThrow = itsThrows[ball++];
-            // Check for strike
-            if (firstThrow == 10) {
-                score += 10 + itsThrows[ball] + itsThrows[ball + 1];
-            } else  {
-                final int secondThrow = itsThrows[ball++];
-                frameScore = firstThrow + secondThrow;
-                // Spare needs next frame's first throw
-                if (frameScore == 10) {
-                    score += frameScore + itsThrows[ball];
-                } else {
-                    score += frameScore;
-                }
+            if (strike()) {
+                score += 10 + nextTwoBallsForStrike();
+                ball++;
+            } else if (spare()) {
+                score += 10 + nextBallForSpare();
+                ball += 2;
+            } else {
+                score += twoBallsInFrame();
+                ball += 2;
             }
         }
         return score;
+    }
+
+    private int nextTwoBallsForStrike() {
+        return itsThrows[ball+1] + itsThrows[ball + 2];
+    }
+
+    private boolean strike() {
+        return itsThrows[ball] == 10;
+    }
+
+    private int twoBallsInFrame() {
+        return itsThrows[ball] + itsThrows[ball + 1];
+    }
+
+    private int nextBallForSpare() {
+        return itsThrows[ball + 2];
+    }
+
+    private boolean spare() {
+        return (itsThrows[ball] + itsThrows[ball + 1]) == 10;
     }
 
     public int getCurrentFrame() {
